6f639ee3de81d245ca61eeb527078858cb297eb0
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-10-28 23:15:49 +0530

    user search added �

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns =[
     path('',views.index,name='index'),
     path('signup',views.signup,name='signup'),
     path('signin',views.signin,name='signin'),
+    path('search', views.search, name='search'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('like-post', views.like_post, name='like-post'),
     path('follow', views.follow, name='follow'),
