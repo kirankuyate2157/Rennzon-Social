6b5706c1f4271ef5d0678a626c2039c778c96530
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-10-29 21:20:31 +0530

    deployment script done

--- socialbook/urls.py
+++ socialbook/urls.py
@@ -1,4 +1,4 @@
-"""social_book URL Configuration
+"""socialbook URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
